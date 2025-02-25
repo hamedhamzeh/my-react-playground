f2d96222f1bf412ced4e6144ee2dcef47178048d
hamed hamze <hamed.hamze2212@gmail.com>
2025-02-25 13:15:28 +0330

    Add todo list comonent.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useState } from "react";
-import ExpenseList from "./components/expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-import categories from "./components/expense-tracker/categories";
+import TodoList from "./components/react-query/TodoList";
 
 function App() {
-    const [selectedCategory, setSelectedCategory] = useState("");
-
-    const [expenses, setExpenses] = useState([
-        { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-        { id: 2, description: "bbb", amount: 20, category: "Utilities" },
-        { id: 3, description: "ccc", amount: 50, category: "Groceries" },
-        { id: 4, description: "ddd", amount: 5, category: "Groceries" }
-    ]);
-
-    const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses;
-
     return (
         <div className="container">
-            <ExpenseForm
-                onSubmit={(expense) => setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])}
-            ></ExpenseForm>
-            <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)}></ExpenseFilter>
-            <ExpenseList
-                expenses={visibleExpenses}
-                onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-            ></ExpenseList>
+            <TodoList />
         </div>
     );
 }
